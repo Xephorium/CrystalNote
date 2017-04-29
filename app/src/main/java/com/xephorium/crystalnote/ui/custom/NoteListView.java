4f4b3a7c35a7022d007d58a31a6ac423cbdf4dfe
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2017-04-29 12:13:34 -0500

    Implement NoteListView in SelectionActivity

--- app/src/main/java/com/xephorium/crystalnote/ui/custom/NoteListView.java
+++ app/src/main/java/com/xephorium/crystalnote/ui/custom/NoteListView.java
@@ -16,23 +16,169 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.xephorium.crystalnote.R;
+import com.xephorium.crystalnote.data.model.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NoteListView extends SwipeRefreshLayout {
 
+    @BindView(R.id.new_note_header)
+    TextView newNoteHeader;
+
+    @BindView(R.id.new_note_list_view)
+    ListView newNoteListView;
+
+    @BindView(R.id.old_note_header)
+    TextView oldNoteHeader;
+
+    @BindView(R.id.old_note_list_view)
+    ListView oldNoteListView;
+
+    private NoteListViewListener noteListViewListener;
+
     public NoteListView(Context context) {
         super(context, null);
+        buildNoteListView(context);
+        ButterKnife.bind(this);
     }
 
     public NoteListView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        buildNoteListView(context);
+        ButterKnife.bind(this);
     }
 
     private void buildNoteListView(Context context) {
         LayoutInflater layoutInflater = LayoutInflater.from(context);
         View noteListViewLayout = layoutInflater.inflate(R.layout.note_list_view_layout, null);
         this.addView(noteListViewLayout);
+
+        this.setNoteListViewListener(getDefaultNoteListViewListener());
+        this.setOnRefreshListener(getOnRefreshListener());
+    }
+
+    public void populateNoteList(List<Note> newNotes, List<Note> oldNotes) {
+        if (newNotes.size() > 0 && oldNotes.size() > 0) {
+            newNoteHeader.setVisibility(View.VISIBLE);
+            newNoteListView.setVisibility(View.VISIBLE);
+            newNoteListView.setAdapter(getNoteListAdapter(newNotes));
+            oldNoteHeader.setVisibility(View.VISIBLE);
+            oldNoteListView.setVisibility(View.VISIBLE);
+            oldNoteListView.setAdapter(getNoteListAdapter(oldNotes));
+        } else if (newNotes.size() > 0) {
+            newNoteHeader.setVisibility(View.GONE);
+            newNoteListView.setVisibility(View.VISIBLE);
+            newNoteListView.setAdapter(getNoteListAdapter(newNotes));
+            oldNoteHeader.setVisibility(View.GONE);
+            oldNoteListView.setVisibility(View.GONE);
+            oldNoteListView.setAdapter(getNoteListAdapter(new ArrayList<Note>()));
+        } else if (oldNotes.size() > 0) {
+            newNoteHeader.setVisibility(View.GONE);
+            newNoteListView.setVisibility(View.GONE);
+            newNoteListView.setAdapter(getNoteListAdapter(new ArrayList<Note>()));
+            oldNoteHeader.setVisibility(View.GONE);
+            oldNoteListView.setVisibility(View.VISIBLE);
+            oldNoteListView.setAdapter(getNoteListAdapter(oldNotes));
+        } else {
+            newNoteHeader.setVisibility(View.GONE);
+            newNoteListView.setVisibility(View.GONE);
+            newNoteListView.setAdapter(getNoteListAdapter(new ArrayList<Note>()));
+            oldNoteHeader.setVisibility(View.GONE);
+            oldNoteListView.setVisibility(View.GONE);
+            oldNoteListView.setAdapter(getNoteListAdapter(new ArrayList<Note>()));
+        }
+        newNoteListView.setLayoutParams(getScrollViewHeightParams(newNoteListView));
+        oldNoteListView.setLayoutParams(getScrollViewHeightParams(oldNoteListView));
+    }
+
+    public void setNoteListViewListener(NoteListViewListener noteListViewListener) {
+        this.noteListViewListener = noteListViewListener;
+    }
+
+    public NoteListViewListener getNoteListViewListener() {
+        return this.noteListViewListener;
+    }
+
+    private NoteListViewListener getDefaultNoteListViewListener() {
+        return new NoteListViewListener() {
+            @Override
+            public void onNoteClick(Note note) {
+                // Default Behavior; Do Nothing
+            }
+
+            @Override
+            public void onNoteLongClick(Note note) {
+                // Default Behavior; Do Nothing
+            }
+
+            @Override
+            public void onNoteListRefresh() {
+                // Default Behavior; Do Nothing
+            }
+        };
+    }
+
+    private NoteListAdapter getNoteListAdapter(final List<Note> notes) {
+        return new NoteListAdapter(notes) {
+            @Override
+            public View.OnClickListener getOnClickListener(final int position) {
+                return new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        noteListViewListener.onNoteClick(notes.get(position));
+                    }
+                };
+            }
+
+            @Override
+            public View.OnLongClickListener getOnLongClickListener(final int position) {
+                return new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View view) {
+                        noteListViewListener.onNoteLongClick(notes.get(position));
+                        return true;
+                    }
+                };
+            }
+        };
+    }
+
+    private SwipeRefreshLayout.OnRefreshListener getOnRefreshListener() {
+        return new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                noteListViewListener.onNoteListRefresh();
+                setRefreshing(false);
+            }
+        };
+    }
+
+    private ViewGroup.LayoutParams getScrollViewHeightParams(ListView listView) {
+        int listViewHeight = 0;
+        for (int x = 0; x < listView.getAdapter().getCount(); x++) {
+            View listItem = listView.getAdapter().getView(x, null, listView);
+            listItem.measure(0, 0);
+            listViewHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = listViewHeight + (listView.getDividerHeight() * (listView.getAdapter().getCount() - 1));
+        return params;
+    }
+
+    public interface NoteListViewListener {
+        void onNoteClick(Note note);
+        void onNoteLongClick(Note note);
+        void onNoteListRefresh();
     }
 
 }
--- app/src/main/java/com/xephorium/crystalnote/ui/selection/SelectionActivity.java
+++ app/src/main/java/com/xephorium/crystalnote/ui/selection/SelectionActivity.java
@@ -1,21 +1,15 @@
 package com.xephorium.crystalnote.ui.selection;
 
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.xephorium.crystalnote.R;
 import com.xephorium.crystalnote.data.model.Note;
 import com.xephorium.crystalnote.ui.base.BaseActivity;
-import com.xephorium.crystalnote.ui.custom.NoteListAdapter;
+import com.xephorium.crystalnote.ui.custom.NoteListView;
 import com.xephorium.crystalnote.ui.custom.NoteToolbar;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -27,20 +21,8 @@ public class SelectionActivity extends BaseActivity implements SelectionView {
     @BindView(R.id.selection_toolbar)
     NoteToolbar toolbar;
 
-    @BindView(R.id.new_note_header)
-    TextView newNoteHeader;
-
-    @BindView(R.id.new_note_list_view)
-    ListView newNoteListView;
-
-    @BindView(R.id.old_note_header)
-    TextView oldNoteHeader;
-
-    @BindView(R.id.old_note_list_view)
-    ListView oldNoteListView;
-
-    @BindView(R.id.swipe_refresh_layout)
-    SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.note_list_view)
+    NoteListView noteListView;
 
     SelectionPresenter presenter;
 
@@ -56,7 +38,8 @@ public class SelectionActivity extends BaseActivity implements SelectionView {
         ButterKnife.bind(this);
 
         presenter = new SelectionPresenter(this);
-        swipeRefreshLayout.setOnRefreshListener(getOnRefreshListener());
+        noteListView.setNoteListViewListener(getSelectionNoteListViewListener());
+        toolbar.setNoteToolbarListener(getSelectionNoteToolbarListener());
     }
 
     @Override
@@ -78,95 +61,17 @@ public class SelectionActivity extends BaseActivity implements SelectionView {
         toolbar.setTitle(R.string.selection_title);
         toolbar.setLeftButtonImage(R.drawable.icon_back);
         toolbar.setRightButtonImage(R.drawable.icon_search);
-        toolbar.setNoteToolbarListener(getSelectionNoteToolbarListener());
         return toolbar;
     }
 
     @Override
     public void populateNoteList(List<Note> newNotes, List<Note> oldNotes) {
-        if (newNotes.size() > 0 && oldNotes.size() > 0) {
-            newNoteHeader.setVisibility(View.VISIBLE);
-            newNoteListView.setVisibility(View.VISIBLE);
-            newNoteListView.setAdapter(getNoteListAdapter(newNotes));
-            oldNoteHeader.setVisibility(View.VISIBLE);
-            oldNoteListView.setVisibility(View.VISIBLE);
-            oldNoteListView.setAdapter(getNoteListAdapter(oldNotes));
-        } else if (newNotes.size() > 0) {
-            newNoteHeader.setVisibility(View.GONE);
-            newNoteListView.setVisibility(View.VISIBLE);
-            newNoteListView.setAdapter(getNoteListAdapter(newNotes));
-            oldNoteHeader.setVisibility(View.GONE);
-            oldNoteListView.setVisibility(View.GONE);
-            oldNoteListView.setAdapter(getNoteListAdapter(new ArrayList<Note>()));
-        } else if (oldNotes.size() > 0) {
-            newNoteHeader.setVisibility(View.GONE);
-            newNoteListView.setVisibility(View.GONE);
-            newNoteListView.setAdapter(getNoteListAdapter(new ArrayList<Note>()));
-            oldNoteHeader.setVisibility(View.GONE);
-            oldNoteListView.setVisibility(View.VISIBLE);
-            oldNoteListView.setAdapter(getNoteListAdapter(oldNotes));
-        } else {
-            newNoteHeader.setVisibility(View.GONE);
-            newNoteListView.setVisibility(View.GONE);
-            newNoteListView.setAdapter(getNoteListAdapter(new ArrayList<Note>()));
-            oldNoteHeader.setVisibility(View.GONE);
-            oldNoteListView.setVisibility(View.GONE);
-            oldNoteListView.setAdapter(getNoteListAdapter(new ArrayList<Note>()));
-        }
-        newNoteListView.setLayoutParams(getScrollViewHeightParams(newNoteListView));
-        oldNoteListView.setLayoutParams(getScrollViewHeightParams(oldNoteListView));
+        noteListView.populateNoteList(newNotes, oldNotes);
     }
 
     @Override
     public void closeSelectionActivity() {
         this.finish();
-    }
-
-    public SwipeRefreshLayout.OnRefreshListener getOnRefreshListener() {
-        return new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                presenter.refreshNoteList();
-                swipeRefreshLayout.setRefreshing(false);
-            }
-        };
-    }
-
-    public NoteListAdapter getNoteListAdapter(final List<Note> notes) {
-        return new NoteListAdapter(notes) {
-            @Override
-            public View.OnClickListener getOnClickListener(final int position) {
-                return new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        presenter.handleNoteClick(notes.get(position));
-                    }
-                };
-            }
-
-            @Override
-            public View.OnLongClickListener getOnLongClickListener(final int position) {
-                return new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View view) {
-                        return presenter.handleNoteLongClick(notes.get(position));
-                    }
-                };
-            }
-        };
-    }
-
-    private ViewGroup.LayoutParams getScrollViewHeightParams(ListView listView) {
-        int listViewHeight = 0;
-        for (int x = 0; x < listView.getAdapter().getCount(); x++) {
-            View listItem = listView.getAdapter().getView(x, null, listView);
-            listItem.measure(0, 0);
-            listViewHeight += listItem.getMeasuredHeight();
-        }
-
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = listViewHeight + (listView.getDividerHeight() * (listView.getAdapter().getCount() - 1));
-        return params;
     }
 
     private NoteToolbar.NoteToolbarListener getSelectionNoteToolbarListener() {
@@ -179,6 +84,25 @@ public class SelectionActivity extends BaseActivity implements SelectionView {
             @Override
             public void onRightButtonClick() {
                 Toast.makeText(getApplicationContext(), "Search Notes", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    private NoteListView.NoteListViewListener getSelectionNoteListViewListener() {
+        return new NoteListView.NoteListViewListener() {
+            @Override
+            public void onNoteClick(Note note) {
+                presenter.handleNoteClick(note);
+            }
+
+            @Override
+            public void onNoteLongClick(Note note) {
+                presenter.handleNoteLongClick(note);
+            }
+
+            @Override
+            public void onNoteListRefresh() {
+                presenter.refreshNoteList();
             }
         };
     }
