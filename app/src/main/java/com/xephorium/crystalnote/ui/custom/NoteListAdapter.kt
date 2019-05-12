7a2c98e12086d79e131b96326d48b65db4c14a25
unknown <christopher.a.cruzen@gmail.com>
2019-05-12 01:01:41 -0500

    Update NoteListAdapter

--- app/src/main/java/com/xephorium/crystalnote/ui/custom/NoteListAdapter.kt
+++ app/src/main/java/com/xephorium/crystalnote/ui/custom/NoteListAdapter.kt
@@ -6,12 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
 
 import com.xephorium.crystalnote.R
 import com.xephorium.crystalnote.data.utility.NoteUtility
 import com.xephorium.crystalnote.data.model.Note
+import kotlinx.android.synthetic.main.note_layout.view.*
 
 /*
   NoteListAdapter                                                           05.12.2019
@@ -22,36 +21,22 @@ import com.xephorium.crystalnote.data.model.Note
 
 */
 
-open class NoteListAdapter(noteList: List<Note>) : BaseAdapter() {
+open class NoteListAdapter(private val notes: List<Note>) : BaseAdapter() {
 
-    var notes: List<Note>? = null
-        private set
+    override fun getView(position: Int, noteLayout: View, parent: ViewGroup): View {
 
-    init {
-        notes = noteList
-    }
+        val inflater = parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val layout = inflater.inflate(R.layout.note_layout, parent, false)
 
-    override fun getView(position: Int, noteLayout: View?, parent: ViewGroup): View {
-        var noteLayout = noteLayout
-        if (noteLayout == null) {
-            val inflater = parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            noteLayout = inflater.inflate(R.layout.note_layout, parent, false)
+        layout.note_title.text = notes[position].name
+        layout.note_preview.text = notes[position].preview
+        layout.note_date.text = NoteUtility.getFormattedDate(notes[position])
+        layout.note_icon.setColorFilter(notes[position].color, PorterDuff.Mode.SRC_ATOP)
 
-            val noteTitle = noteLayout!!.findViewById<View>(R.id.note_title) as TextView
-            val notePreview = noteLayout.findViewById<View>(R.id.note_preview) as TextView
-            val noteDate = noteLayout.findViewById<View>(R.id.note_date) as TextView
-            val noteIcon = noteLayout.findViewById<View>(R.id.note_icon) as ImageView
+        layout.setOnClickListener(getOnClickListener(position))
+        layout.setOnLongClickListener(getOnLongClickListener(position))
 
-            noteTitle.text = notes!![position].name
-            notePreview.text = notes!![position].preview
-            noteDate.text = NoteUtility.getFormattedDate(notes!![position])
-            noteIcon.setColorFilter(notes!![position].color, PorterDuff.Mode.SRC_ATOP)
-
-            noteLayout.setOnClickListener(getOnClickListener(position))
-            noteLayout.setOnLongClickListener(getOnLongClickListener(position))
-        }
-
-        return noteLayout
+        return layout
     }
 
     override fun getItemId(position: Int): Long {
@@ -59,20 +44,20 @@ open class NoteListAdapter(noteList: List<Note>) : BaseAdapter() {
     }
 
     override fun getItem(position: Int): Any {
-        return notes!![position]
+        return notes[position]
     }
 
     override fun getCount(): Int {
-        return if (notes != null) notes!!.size else 0
+        return notes.size
     }
 
-    open fun getOnClickListener(position: Int): View.OnClickListener {
+    private fun getOnClickListener(position: Int): View.OnClickListener {
         return View.OnClickListener {
             // Default Behavior; Do Nothing
         }
     }
 
-    open fun getOnLongClickListener(position: Int): View.OnLongClickListener {
+    private fun getOnLongClickListener(position: Int): View.OnLongClickListener {
         return View.OnLongClickListener {
             // Default Behavior; Do Nothing
             true
