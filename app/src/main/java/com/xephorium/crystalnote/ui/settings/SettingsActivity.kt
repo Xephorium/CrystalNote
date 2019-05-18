0cee15cf6703e2ed1522a413809f59657354b4ab
unknown <christopher.a.cruzen@gmail.com>
2019-05-18 15:32:53 -0500

    Update Settings UI/Content

--- app/src/main/java/com/xephorium/crystalnote/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/xephorium/crystalnote/ui/settings/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package com.xephorium.crystalnote.ui.settings
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 
 import com.xephorium.crystalnote.R
 import com.xephorium.crystalnote.ui.custom.NoteToolbar
 import com.xephorium.crystalnote.ui.drawer.DrawerActivity
+import kotlinx.android.synthetic.main.settings_activity_layout.*
 import kotlinx.android.synthetic.main.toolbar_activity_layout.*
+import android.widget.ArrayAdapter
 
 
 class SettingsActivity : DrawerActivity(), SettingsContract.View {
@@ -25,6 +29,8 @@ class SettingsActivity : DrawerActivity(), SettingsContract.View {
         presenter = SettingsPresenter()
 
         setupToolbar()
+        setupThemeSpinner()
+        setupNotePreviewLinesSpinner()
     }
 
     override fun onResume() {
@@ -56,6 +62,40 @@ class SettingsActivity : DrawerActivity(), SettingsContract.View {
             override fun onRightButtonClick() = Unit
             override fun onTextChange(text: String) = Unit
         })
+    }
+
+    private fun setupThemeSpinner() {
+        val themeAdapter = ArrayAdapter<String>(this, R.layout.settings_selector_item, THEMES)
+        themeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        selectorSettingsTheme.adapter = themeAdapter
+        selectorSettingsTheme.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+            override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // TODO - Handle Item Select
+            }
+        }
+        textSettingsThemeLabel.setOnClickListener { selectorSettingsTheme.performClick() }
+    }
+
+    private fun setupNotePreviewLinesSpinner() {
+        val linesAdapter = ArrayAdapter<String>(this, R.layout.settings_selector_item, NOTE_PREVIEW_LINES)
+        linesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        selectorSettingsLines.adapter = linesAdapter
+        selectorSettingsLines.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+            override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // TODO - Handle Item Select
+            }
+        }
+        textSettingsNoteLinesLabel.setOnClickListener { selectorSettingsLines.performClick() }
+    }
+
+
+    /*--- Constants ---*/
+
+    companion object {
+        private val THEMES = listOf("Monochrome", "Blue")
+        private val NOTE_PREVIEW_LINES = listOf("1 Line", "2 Lines", "3 Lines", "4 Lines", "5 Lines")
     }
 
 }
