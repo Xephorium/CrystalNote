c0f0d3a7c53fe919242717da9bab0e9bd62eabf4
unknown <christopher.a.cruzen@gmail.com>
2019-05-19 18:31:08 -0500

    Extract More Colors

--- app/src/main/java/com/xephorium/crystalnote/ui/extensions/ContextExtensions.kt
+++ app/src/main/java/com/xephorium/crystalnote/ui/extensions/ContextExtensions.kt
@@ -3,8 +3,6 @@ package com.xephorium.crystalnote.ui.extensions
 import android.content.Context
 import android.util.TypedValue
 
-
-
 fun Context.getThemeColor(resId: Int) : Int {
     val typedValue = TypedValue()
     theme.resolveAttribute(resId, typedValue, true)
