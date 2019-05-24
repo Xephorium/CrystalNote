8499b20964a4bcbcb59eeff1cb8779ef9cabf859
unknown <christopher.a.cruzen@gmail.com>
2019-05-23 20:16:07 -0500

    Update ThemePreview

--- app/src/main/java/com/xephorium/crystalnote/ui/custom/ThemePreview.kt
+++ app/src/main/java/com/xephorium/crystalnote/ui/custom/ThemePreview.kt
@@ -22,6 +22,7 @@ class ThemePreview : View {
     private var scaleUnit: Int? = null
 
     private var toolbarHeight: Int? = null
+    private var headerHeight: Int? = null
     private var textHeight: Int? = null
     private var actionButtonRadius: Int? = null
 
@@ -60,7 +61,8 @@ class ThemePreview : View {
         }
         if (scaleUnit == null) scaleUnit = (viewWidth!! / 12)
 
-        if (textHeight == null) textHeight = (scaleUnit!! * .5).toInt()
+        if (textHeight == null) textHeight = (scaleUnit!! * .3).toInt()
+        if (headerHeight == null) headerHeight = (scaleUnit!! * .4).toInt()
         if (toolbarHeight == null) toolbarHeight = (scaleUnit!! * 2.25).toInt()
         if (actionButtonRadius == null) actionButtonRadius = (scaleUnit!! * 1).toInt()
 
@@ -120,17 +122,50 @@ class ThemePreview : View {
             canvas?.drawRoundRect(
                     (paddingLarge!!).toFloat(),
                     currentVerticalPosition.toFloat(),
-                    (paddingLarge!! + (scaleUnit!! * 2.5)).toFloat(),
-                    (currentVerticalPosition + textHeight!!).toFloat(),
+                    (paddingLarge!! + (scaleUnit!! * 1.7)).toFloat(),
+                    (currentVerticalPosition + headerHeight!!).toFloat(),
                     CORNER_RADIUS,
                     CORNER_RADIUS,
                     paint
             )
-            currentVerticalPosition += (textHeight!! + paddingLarge!!)
+            currentVerticalPosition += (headerHeight!! + paddingLarge!!)
         }
 
         // Card 1
-        currentVerticalPosition += drawNoteCard(canvas, paint, currentVerticalPosition)
+        currentVerticalPosition += drawNoteCard(canvas, paint, currentVerticalPosition, 5, 4)
+
+        // Card 2
+        currentVerticalPosition += drawNoteCard(canvas, paint, currentVerticalPosition, 4, 2)
+
+        // Header 2
+        if (SHOW_HEADERS) {
+            paint.color = ContextCompat.getColor(context, R.color.lightTextSecondary)
+            canvas?.drawRoundRect(
+                    (paddingLarge!!).toFloat(),
+                    (currentVerticalPosition + paddingMedium!!).toFloat(),
+                    (paddingLarge!! + (scaleUnit!! * 1.3)).toFloat(),
+                    (currentVerticalPosition + paddingMedium!! + headerHeight!!).toFloat(),
+                    CORNER_RADIUS,
+                    CORNER_RADIUS,
+                    paint
+            )
+            currentVerticalPosition += (headerHeight!! + paddingMedium!! + paddingLarge!!)
+        }
+
+        // Card 3
+        currentVerticalPosition += drawNoteCard(canvas, paint, currentVerticalPosition, 6, 3)
+
+        // Card 4
+        currentVerticalPosition += drawNoteCard(canvas, paint, currentVerticalPosition, 3, 1)
+
+        // Card 5
+        currentVerticalPosition += drawNoteCard(canvas, paint, currentVerticalPosition, 5, 4)
+
+        // Card 6
+        currentVerticalPosition += drawNoteCard(canvas, paint, currentVerticalPosition, 4, 2)
+
+        // Card 7
+        currentVerticalPosition += drawNoteCard(canvas, paint, currentVerticalPosition, 6, 3)
 
         // Floating Action Button
         paint.color = ContextCompat.getColor(context, R.color.red500)
@@ -154,18 +189,22 @@ class ThemePreview : View {
     private fun drawNoteCard(
             canvas: Canvas?,
             paint: Paint,
-            currentVerticalPosition: Int
+            currentVerticalPosition: Int,
+            titleLength: Int,
+            previewLines: Int
     ): Int {
 
-        val textLineHeight = textHeight!! + (scaleUnit!! * (.5))
-        val viewHeight = (scaleUnit!! * (1)) + (NOTE_LINES * textLineHeight)
+        val textTitleHeight = textHeight!! + (paddingMedium!! * 2)
+        val textLineHeight = textHeight!! + paddingSmall!!
+        val lines = if (previewLines > MAX_LINES) MAX_LINES else previewLines
+        val viewHeight = textTitleHeight + (lines * textLineHeight) + paddingSmall!!
 
         // Background
-        paint.color = ContextCompat.getColor(context, R.color.white)
+        paint.color = ContextCompat.getColor(context, R.color.lightNoteBackground)
         canvas?.drawRoundRect(
-                (scaleUnit!! * .75).toFloat(),
+                (paddingLarge!!).toFloat(),
                 currentVerticalPosition.toFloat(),
-                (viewWidth!! - (scaleUnit!! * .75)).toFloat(),
+                (viewWidth!! - (paddingLarge!!)).toFloat(),
                 (currentVerticalPosition + viewHeight).toFloat(),
                 CORNER_RADIUS,
                 CORNER_RADIUS,
@@ -173,18 +212,48 @@ class ThemePreview : View {
         )
 
         // Title
-        paint.color = ContextCompat.getColor(context, R.color.white)
+        paint.color = ContextCompat.getColor(context, R.color.lightTextPrimary)
         canvas?.drawRoundRect(
-                (scaleUnit!! * 1.25).toFloat(),
-                (currentVerticalPosition + (scaleUnit!! * .5)).toFloat(),
-                (viewWidth!! - (scaleUnit!! * .75)).toFloat(),
-                (currentVerticalPosition + (scaleUnit!! * .5) + textHeight!!).toFloat(),
+                (paddingLarge!! + paddingMedium!!).toFloat(),
+                (currentVerticalPosition + paddingMedium!!).toFloat(),
+                (paddingLarge!! + paddingMedium!! + (scaleUnit!! * titleLength)).toFloat(),
+                (currentVerticalPosition + paddingMedium!! + textHeight!!).toFloat(),
                 CORNER_RADIUS,
                 CORNER_RADIUS,
                 paint
         )
 
-        return 5
+        // Date
+        if (DATE_TYPE != DateType.NONE) {
+            paint.color = ContextCompat.getColor(context, R.color.lightTextTertiary)
+            canvas?.drawRoundRect(
+                    (viewWidth!! - (paddingLarge!! + paddingMedium!!) - (scaleUnit!! * 1.7)).toFloat(),
+                    (currentVerticalPosition + paddingMedium!!).toFloat(),
+                    (viewWidth!! - (paddingLarge!! + paddingMedium!!)).toFloat(),
+                    (currentVerticalPosition + paddingMedium!! + textHeight!!).toFloat(),
+                    CORNER_RADIUS,
+                    CORNER_RADIUS,
+                    paint
+            )
+        }
+
+        // Lines
+        paint.color = ContextCompat.getColor(context, R.color.lightTextSecondary)
+        for (x in 0 until lines) {
+            val lineEndPosition = viewWidth!! - (paddingLarge!! + paddingMedium!!)
+            val baseHeight = currentVerticalPosition + textTitleHeight + (x * textLineHeight)
+            canvas?.drawRoundRect(
+                    (paddingLarge!! + paddingMedium!!).toFloat(),
+                    (baseHeight).toFloat(),
+                    (lineEndPosition).toFloat(),
+                    (baseHeight + textHeight!!).toFloat(),
+                    CORNER_RADIUS,
+                    CORNER_RADIUS,
+                    paint
+            )
+        }
+
+        return viewHeight + paddingMedium!!
     }
 
 
@@ -194,9 +263,9 @@ class ThemePreview : View {
         private const val STROKE_WIDTH = 3.toFloat()
         private const val CORNER_RADIUS = 3.toFloat()
 
-        private const val SHOW_HEADERS = true
+        private const val SHOW_HEADERS = false
         private const val SHOW_COLOR_BAR = true
+        private const val MAX_LINES = 2
         private val DATE_TYPE = DateType.DYNAMIC
-        private const val NOTE_LINES = 3
     }
 }
