aa7ac1f9a72abcd88f1edb4257d7cc3779a5f38f
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2020-03-01 13:10:06 -0600

    Cleanup ColorOrb

--- app/src/main/java/com/xephorium/crystalnote/ui/custom/ColorOrb.kt
+++ app/src/main/java/com/xephorium/crystalnote/ui/custom/ColorOrb.kt
@@ -9,6 +9,7 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.core.graphics.ColorUtils
 import com.xephorium.crystalnote.data.model.CrystalNoteTheme
+import com.xephorium.crystalnote.data.utility.ColorUtility
 
 
 class ColorOrb : View {
@@ -17,11 +18,16 @@ class ColorOrb : View {
     /*--- Variable Declarations ---*/
 
     private val paint = Paint()
+
     private var viewHeight: Int? = null
     private var viewWidth: Int? = null
     private var desiredViewHeight: Int? = null
+
     private var theme = CrystalNoteTheme.default(context)
     private var orbColor = DEFAULT_ORB_COLOR
+    private var orbContrast = 2.0
+    private var padding: Double = 0.0
+    private var useContrastOutline = true
 
 
     /*--- Constructors ---*/
@@ -58,13 +64,22 @@ class ColorOrb : View {
 
     override fun onDraw(canvas: Canvas?) {
 
+        // Calculate Padding
+        val pad = (1.0 - padding)
+
+        // Set Outline Paint Color
+        if (orbContrast < CONTRAST_THRESHOLD && useContrastOutline) {
+            paint.color = ColorUtils.setAlphaComponent(theme.colorTextPrimary, 40)
+        } else {
+            paint.color = orbColor
+        }
+
         // Outline
-        paint.color = ColorUtils.setAlphaComponent(theme.colorTextSecondary, 125)
         paint.style = STROKE
         canvas?.drawCircle(
             (viewWidth!! / 2.0).toFloat(),
             (viewHeight!! / 2.0).toFloat(),
-            ((viewWidth!! * RADIUS_COLOR) / 2.0).toFloat(),
+            ((viewWidth!! * RADIUS_COLOR * pad) / 2.0).toFloat(),
             paint
         )
 
@@ -74,7 +89,7 @@ class ColorOrb : View {
         canvas?.drawCircle(
             (viewWidth!! / 2.0).toFloat(),
             (viewHeight!! / 2.0).toFloat(),
-            ((viewWidth!! * RADIUS_COLOR) / 2.0).toFloat(),
+            ((viewWidth!! * RADIUS_COLOR * pad) / 2.0).toFloat(),
             paint
         )
 
@@ -91,12 +106,34 @@ class ColorOrb : View {
 
     fun setColor(color: Int) {
         orbColor = color
+        orbContrast = ColorUtility.calculateContrastRatio(theme.colorNoteBackground, color)
         invalidate()
     }
 
-    fun setSize(sizeResource: Int) {
-        desiredViewHeight = resources.getDimensionPixelSize(sizeResource).toInt()
+    fun getColor(): Int {
+        return orbColor
+    }
+
+    fun setSizeByResourceId(sizeResource: Int) {
+        desiredViewHeight = resources.getDimensionPixelSize(sizeResource)
         invalidate()
+    }
+
+    fun setSizeByPixelValue(pixelValue: Int) {
+        desiredViewHeight = pixelValue
+        invalidate()
+    }
+
+    fun setPadding(newPadding: Double) {
+        padding = newPadding
+    }
+
+    fun enableContrastOutline() {
+        useContrastOutline = true
+    }
+
+    fun disableContrastOutline() {
+        useContrastOutline = false
     }
 
 
@@ -104,6 +141,7 @@ class ColorOrb : View {
 
     companion object {
         val DEFAULT_ORB_COLOR = Color.parseColor("#000000")
+        const val CONTRAST_THRESHOLD = 1.1
         const val RADIUS_COLOR = 0.9
         const val OUTLINE_WIDTH = (5.0).toFloat()
     }
