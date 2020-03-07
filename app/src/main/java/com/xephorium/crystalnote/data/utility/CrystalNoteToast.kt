688d469c0a56a3f7a6bf677b7d17c89182a5960c
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2020-03-07 00:57:55 -0600

    Fix Toast Padding

--- app/src/main/java/com/xephorium/crystalnote/data/utility/CrystalNoteToast.kt
+++ app/src/main/java/com/xephorium/crystalnote/data/utility/CrystalNoteToast.kt
@@ -1,7 +1,9 @@
 package com.xephorium.crystalnote.data.utility
 
 import android.content.Context
-import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.LinearLayout.LayoutParams.WRAP_CONTENT
 import android.widget.TextView
 import android.widget.Toast
 import com.xephorium.crystalnote.R
@@ -11,30 +13,31 @@ import com.xephorium.crystalnote.data.model.CrystalNoteTheme
 object CrystalNoteToast {
 
     fun showShort(context: Context, string: String) {
-        val view: View
-        val textView: TextView
-        val toast: Toast = Toast.makeText(context, string, Toast.LENGTH_SHORT)
-
-        view = toast.view
-        view.setBackgroundResource(R.drawable.toast_background)
-        textView = view.findViewById(android.R.id.message)
-        textView.setTextColor(CrystalNoteTheme.fromCurrentTheme(context).colorTextInvertedPrimary)
-        textView.setShadowLayer(0f, 0f, 0f, 0)
-
-        toast.show()
+        showToast(context, string, Toast.LENGTH_SHORT)
     }
 
     fun showLong(context: Context, string: String) {
-        val view: View
-        val textView: TextView
-        val toast: Toast = Toast.makeText(context, string, Toast.LENGTH_LONG)
+        showToast(context, string, Toast.LENGTH_LONG)
+    }
 
-        view = toast.view
+    private fun showToast(context: Context, string: String, length: Int) {
+
+        // Create Necessary Variables
+        val toast: Toast = Toast.makeText(context, string, length)
+        val view = toast.view
+        val textView = view.findViewById(android.R.id.message) as TextView
+        val padding = context.resources.getDimensionPixelSize(R.dimen.paddingBig)
+        val oldLayoutParams = textView.layoutParams as ViewGroup.MarginLayoutParams
+        val newLayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        newLayoutParams.setMargins( 0, oldLayoutParams.topMargin, 0, oldLayoutParams.bottomMargin)
+
+        // Configure Toast
         view.setBackgroundResource(R.drawable.toast_background)
-        textView = view.findViewById(android.R.id.message)
         textView.setTextColor(CrystalNoteTheme.fromCurrentTheme(context).colorTextInvertedPrimary)
-        textView.setShadowLayer(0f, 0f, 0f, 0)
+        textView.layoutParams = newLayoutParams
+        textView.setPadding(padding, 0, padding, 0)
 
+        // Show Toast
         toast.show()
     }
 }
