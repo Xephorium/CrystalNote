d95cbe8a6ae7957e3b8291fee2d8af85789300ed
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2023-12-12 02:21:31 -0600

    Fix Custom Toasts

--- app/src/main/java/com/xephorium/crystalnote/data/utility/CrystalNoteToast.kt
+++ app/src/main/java/com/xephorium/crystalnote/data/utility/CrystalNoteToast.kt
@@ -1,13 +1,11 @@
 package com.xephorium.crystalnote.data.utility
 
 import android.content.Context
-import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.LinearLayout.LayoutParams.WRAP_CONTENT
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.widget.TextView
 import android.widget.Toast
 import com.xephorium.crystalnote.R
-import com.xephorium.crystalnote.data.model.CrystalNoteTheme
 
 
 object CrystalNoteToast {
@@ -21,24 +19,14 @@ object CrystalNoteToast {
     }
 
     private fun showToast(context: Context, string: String, length: Int) {
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+        val layout = inflater.inflate(R.layout.toast_themed, null)
+        layout.findViewById<TextView>(R.id.toastText).text = string
 
-        // Create Necessary Variables
-        val toast: Toast = Toast.makeText(context, string, length)
-        toast.view?.run {
-            val textView = findViewById<TextView>(android.R.id.message)
-            val padding = context.resources.getDimensionPixelSize(R.dimen.paddingBig)
-            val oldLayoutParams = textView.layoutParams as ViewGroup.MarginLayoutParams
-            val newLayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-            newLayoutParams.setMargins( 0, oldLayoutParams.topMargin, 0, oldLayoutParams.bottomMargin)
-
-            // Configure Toast
-            setBackgroundResource(R.drawable.toast_background)
-            textView.setTextColor(CrystalNoteTheme.fromCurrentTheme(context).colorTextInvertedPrimary)
-            textView.layoutParams = newLayoutParams
-            textView.setPadding(padding, 0, padding, 0)
-
-            // Show Toast
-            toast.show()
-        }
+        val toast = Toast.makeText(context, string, length)
+        toast.setGravity(Gravity.BOTTOM, 0, 100)
+        toast.duration = Toast.LENGTH_LONG
+        toast.setView(layout)
+        toast.show()
     }
 }
