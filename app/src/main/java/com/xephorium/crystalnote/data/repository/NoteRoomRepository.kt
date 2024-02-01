57ddeeaf2601108bc333dd6bf7685c13a946239c
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2024-01-31 18:55:36 -0600

    Update GitHub Showcase

--- app/src/main/java/com/xephorium/crystalnote/data/repository/NoteRoomRepository.kt
+++ app/src/main/java/com/xephorium/crystalnote/data/repository/NoteRoomRepository.kt
@@ -149,7 +149,7 @@ class NoteRoomRepository(context: Context) {
                 id = Note.NO_NOTE,
                 name = "Note",
                 contents = "• List Item #5\n• List Item #6\n\n• List Item #7\n• List Item #8\n\nLorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor.",
-                preview = "• List Item #5",
+                preview = "",
                 date = Date(DATE_OF_FIRST_COMMIT + (ONE_MINUTE * 5)),
                 color = HomePreviewView.NOTE_COLORS[0],
                 archived = false
@@ -158,7 +158,7 @@ class NoteRoomRepository(context: Context) {
                 id = Note.NO_NOTE,
                 name = "Beautiful Vistas",
                 contents = "• Shipyard, Halo Reach\n• All of Skyrim\n• Winterfell, The Forest",
-                preview = "• Shipyard, Halo Reach",
+                preview = "",
                 date = Date(DATE_OF_FIRST_COMMIT + (ONE_MINUTE * 4)),
                 color = HomePreviewView.NOTE_COLORS[1],
                 archived = false
@@ -167,7 +167,7 @@ class NoteRoomRepository(context: Context) {
                 id = Note.NO_NOTE,
                 name = "Shopping List",
                 contents = "• Bread\n• Milk\n• Eggs\n• Sugar",
-                preview = "• Bread",
+                preview = "",
                 date = Date(DATE_OF_FIRST_COMMIT + (ONE_MINUTE * 3)),
                 color = HomePreviewView.NOTE_COLORS[2],
                 archived = false
@@ -176,7 +176,7 @@ class NoteRoomRepository(context: Context) {
                 id = Note.NO_NOTE,
                 name = "Reasons To Code",
                 contents = "• Puzzle Solving!\n• Free To Learn\n• Hones Critical Thinking\n• It Pays Well",
-                preview = "• Puzzle Solving",
+                preview = "",
                 date = Date(DATE_OF_FIRST_COMMIT + (ONE_MINUTE * 2)),
                 color = HomePreviewView.NOTE_COLORS[3],
                 archived = false
@@ -185,20 +185,29 @@ class NoteRoomRepository(context: Context) {
                 id = Note.NO_NOTE,
                 name = "Cities to Visit",
                 contents = "• Chicago\n• New York\n• Seattle\n• San Francisco",
-                preview = "• Chicago",
+                preview = "",
                 date = Date(DATE_OF_FIRST_COMMIT + ONE_MINUTE),
                 color = HomePreviewView.NOTE_COLORS[4],
                 archived = false
             ),
             Note(
                 id = Note.NO_NOTE,
-                name = "A Final Note",
-                contents = "Building this app has been challenging. There were more than a few moments when I was ready to throw in the towel and accept that I'd learned enough. But each successfully rendered list item, formatted to responsive perfection has been a reminder of why I started: to create something beautiful. The world deserves a clean notes app for Android. It's about time someone put in the hours to code one.",
-                preview = "Building this app has been challenging.",
+                name = "Lorem Ipsum",
+                contents = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                preview = "",
                 date = Date(DATE_OF_FIRST_COMMIT),
                 color = HomePreviewView.NOTE_COLORS[6],
                 archived = false
-            )
+            ),
+//            Note(
+//                id = Note.NO_NOTE,
+//                name = "A Final Note",
+//                contents = "Building this app has been challenging. There were more than a few moments when I was ready to throw in the towel and accept that I'd learned enough. But each successfully rendered list item, formatted to responsive perfection has been a reminder of why I started: to create something beautiful. The world deserves a clean notes app for Android. It's about time someone put in the hours to code one.",
+//                preview = "",
+//                date = Date(DATE_OF_FIRST_COMMIT),
+//                color = HomePreviewView.NOTE_COLORS[6],
+//                archived = false
+//            )
         )
     }
 }
